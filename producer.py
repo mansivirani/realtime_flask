5b91c2707216b2cb136aeb458b638cb0bd8b08ac
Mansi Virani <mansi.virani@gmail.com>
2017-06-06 17:35:51 -0400

    deleted unnecessary files

--- producer.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaProducer
-
-
-class Producer:
-    producer = KafkaProducer(bootstrap_servers='113.58.187.142:9092')
-    def send_data(self, topic, msg):
-        self.producer.send(topic, value=msg.encode())
-
